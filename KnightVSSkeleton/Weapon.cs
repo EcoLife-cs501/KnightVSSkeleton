5d0c5a64ab9c83b8a29a0fb3f32ab448ebf5801a
Host Spirit <host_spirit@mail.ru>
2017-01-24 21:05:13 +0300

    Closes #1: Добавлен класс Weapon()
    У него есть параметры MaxDamage & MinDamage, сделанные как Properties.

---
+++ KnightVSSkeleton/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightVSSkeleton
+{
+    public class Weapon
+    {
+
+        public Weapon()
+        {
+            this.MinDamage = 10;
+            this.MaxDamage = 25;
+        }
+
+        virtual public int MinDamage { get; private set; }
+        virtual public int MaxDamage { get; private set; }
+
+
+    }
+}
